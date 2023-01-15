4ccfa951a57c42d2c23990e95d1c31977e6c7ab0
Alexander Gavrilyuk <one1970zero@mail.ru>
2023-01-15 12:34:09 +0300

    logo add

--- app/src/main/java/ru/alexfox/androidbook_test/SelectLanguageActivity.kt
+++ app/src/main/java/ru/alexfox/androidbook_test/SelectLanguageActivity.kt
@@ -3,9 +3,9 @@ package ru.alexfox.androidbook_test
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class SelectLanguageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_select_language)
     }
 }
